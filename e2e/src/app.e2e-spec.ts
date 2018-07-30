302eaf961e9fdf54e5e352d2dab49dfd6039c9be
Dan Harris <Dan.Harris@hfoh.com.au>
2018-07-30 23:26:30 +0930

    feat(base-tests): added some basic e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,38 @@
+import { browser } from 'protractor';
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('ngx-loadable-component test app', async () => {
   let page: AppPage;
+  browser.waitForAngularEnabled(true);
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should contain loadable component', () => {
+    expect(page.getLoadableComponent()).toBeTruthy();
+  });
+
+  describe('when no component loaded', () => {
+    it('should show transcluded content', () => {
+      expect(page.getPlaceholder()).toBeTruthy();
+    });
+
+    it('should hide loadable components', () => {
+      expect(page.getLoadableChildComponent().isPresent()).toBeFalsy();
+    });
+  });
+
+  describe('when component loaded', async () => {
+    it('should hide transcluded content', () => {
+      page.clickLoadButton();
+      expect(page.getPlaceholder().isPresent()).toBeFalsy();
+    });
+
+    it('should show loadable components', () => {
+      page.clickLoadButton();
+      expect(page.getLoadableChildComponent()).toBeTruthy();
+    });
   });
 });
--- projects/ngx-loadable-component/src/lib/services/loadable.service.ts
+++ projects/ngx-loadable-component/src/lib/services/loadable.service.ts
@@ -1,35 +1,29 @@
 import { ComponentFactory, Inject, Injectable, Injector, NgModuleFactory, NgModuleFactoryLoader, NgModuleRef } from '@angular/core';
-import { Observable, from as ObservableFrom, throwError as ObservableThrowError } from 'rxjs';
-
+import { from as ObservableFrom, Observable, throwError as ObservableThrowError } from 'rxjs';
 import { LOADABLE_COMPONENT, LOADABLE_MANIFESTS, LoadableManifest } from '../models/loadable-manifest.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadableService {
+  constructor(@Inject(LOADABLE_MANIFESTS) private manifests: Array<LoadableManifest>, private loader: NgModuleFactoryLoader, private injector: Injector) {}
 
-  constructor(
-    @Inject(LOADABLE_MANIFESTS) private manifests: Array<LoadableManifest>,
-    private loader: NgModuleFactoryLoader,
-    private injector: Injector
-  ) { }
-
-  /** Retrieve a ComponentFactory, based on the specified componentId (defined in the LoadableManifest array). */
+  /**
+   * Retrieve a ComponentFactory, based on the specified componentId (defined in the LoadableManifest array).
+   */
   getComponentFactory<T>(componentId: string, injector?: Injector): Observable<ComponentFactory<T>> {
-
     const manifest: LoadableManifest = this.manifests.find((availableManifest: LoadableManifest) => availableManifest.componentId === componentId);
 
     if (!manifest) return ObservableThrowError(`DynamicComponentLoader: Unknown componentId "${componentId}"`);
 
-    const componentPromise: Promise<ComponentFactory<any>> = this.loader.load(manifest.loadChildren)
-      .then((ngModuleFactory: NgModuleFactory<any>) => {
-        const moduleRef: NgModuleRef<any> = ngModuleFactory.create(injector || this.injector);
-        const dynamicComponentType: any = moduleRef.injector.get(LOADABLE_COMPONENT);
+    const componentPromise: Promise<ComponentFactory<any>> = this.loader.load(manifest.loadChildren).then((ngModuleFactory: NgModuleFactory<any>) => {
+      const moduleRef: NgModuleRef<any> = ngModuleFactory.create(injector || this.injector);
+      const dynamicComponentType: any = moduleRef.injector.get(LOADABLE_COMPONENT);
 
-        if (!dynamicComponentType) throw new Error(`DynamicComponentLoader: Dynamic module for componentId "${componentId}" does not contain LOADABLE_COMPONENT as a provider.`);
+      if (!dynamicComponentType) throw new Error(`DynamicComponentLoader: Dynamic module for componentId "${componentId}" does not contain LOADABLE_COMPONENT as a provider.`);
 
-        return moduleRef.componentFactoryResolver.resolveComponentFactory<T>(dynamicComponentType);
-      });
+      return moduleRef.componentFactoryResolver.resolveComponentFactory<T>(dynamicComponentType);
+    });
 
     return ObservableFrom(componentPromise);
   }
